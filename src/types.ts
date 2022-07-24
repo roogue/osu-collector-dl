6412a2dee5b01160bffd4a96d55c9dc4cb254ee5
roogue <57899698+roogue@users.noreply.github.com>
2022-07-24 12:33:08 +0800

    Update Typings

--- src/types.ts
+++ src/types.ts
@@ -1,6 +1,50 @@
-export interface BeatMapSet {
+export interface BeatMap {
+  checksum: string;
   id: number;
-  beatmaps: object[];
+}
+export interface BeatMapSet {
+  beatmaps: BeatMap[];
+  id: number;
 }
 
-export type ResponseJson = Record<string, any>;
+export interface Collection {
+  beatmapIds: BeatMap[];
+  beatmapsets: BeatMapSet[];
+  beatmapCount: number;
+  id: number;
+  name: string;
+  uploader: {
+    username: string;
+  };
+}
+
+export interface BeatMapV2 {
+  checksum: string;
+  id: number;
+  beatmapset_id: number;
+  beatmapset: BeatMapSetV2;
+  version: string;
+  mode: Mode;
+  difficulty_rating: number;
+}
+
+export interface BeatMapSetV2 {
+  artist: string;
+  title: string;
+  creator: string;
+}
+
+export interface BeatMapV2ResData {
+  nextPageCursor: number;
+  hasMore: boolean;
+  beatmaps: BeatMapV2[];
+}
+
+export type Mode = "taiko" | "osu" | "fruits" | "mania";
+
+export enum ModeByte {
+  "osu" = 0,
+  "taiko" = 1,
+  "fruits" = 2,
+  "mania" = 3,
+}
