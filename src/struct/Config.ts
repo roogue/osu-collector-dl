db761a66c7ab4038eb59e4d4a4e2e201ae687815
roogue <57899698+roogue@users.noreply.github.com>
2022-07-24 12:33:55 +0800

    Fix handling default value

--- src/struct/Config.ts
+++ src/struct/Config.ts
@@ -1,28 +1,45 @@
 import { existsSync, writeFileSync } from "fs";
+import Util from "../util";
 
 export default class Config {
   parallel: boolean;
-  osuCollector_url: string;
-  osuMirror_url: string;
+  osuCollectorApiUrl: string;
+  osuMirrorApiUrl: string;
   dl_impulse_rate: number;
   directory: string;
+  mode: number;
   static readonly configFilePath = "./config.json";
 
   constructor(object?: Record<string, any>) {
     // Osucollector's base url
-    this.osuCollector_url = "https://osucollector.com/";
+    this.osuCollectorApiUrl = "https://osucollector.com/api/collections/";
 
     // Osumirror's api url for download beatmap
-    this.osuMirror_url = "https://api.chimu.moe/v1/";
+    this.osuMirrorApiUrl = "https://api.chimu.moe/v1/";
 
     // Whether download process should be done in parallel
-    this.parallel = object?.parallel ?? true;
+    this.parallel = Util.isBoolean(object?.parallel) ? object!.parallel : true;
 
     // How many urls should be downloaded in parallel at once
-    this.dl_impulse_rate = object?.dl_impulse_rate ?? 10;
+    this.dl_impulse_rate = !isNaN(Number(object?.dl_impulse_rate))
+      ? Number(object!.dl_impulse_rate)
+      : 10;
 
     // Directory to save beatmaps
-    this.directory = object?.directory ?? process.cwd();
+    this.directory = object?.directory
+      ? String(object?.directory)
+      : process.cwd();
+
+    // Mode
+    // 1: Download BeatmapSet
+    // 2: Download BeatmapSet + Generate .osdb
+    if (object?.mode) {
+      const mode = Number(object.mode);
+      // Mode should be 1 or 2
+      ![1, 2].includes(mode) ? (this.mode = 1) : (this.mode = mode);
+    } else {
+      this.mode = 1;
+    }
   }
 
   static generateConfig(): Config {
@@ -33,6 +50,7 @@ export default class Config {
           parallel: true,
           dl_impulse_rate: 10,
           directory: process.cwd(),
+          mode: 1,
         })
       );
     }
