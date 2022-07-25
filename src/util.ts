25344782380a0249571130acd07eebc3d8c478a1
roogue <57899698+roogue@users.noreply.github.com>
2022-07-25 09:23:08 +0800

    Add util function

--- src/util.ts
+++ src/util.ts
@@ -2,4 +2,9 @@ export default class Util {
   static isBoolean(obj: any): boolean {
     return !!obj === obj;
   }
+
+  static replaceForbiddenChars(str: string): string {
+    const regex = /[ \/<>:"\|?*]+/g;
+    return str.replace(regex, "");
+  }
 }
