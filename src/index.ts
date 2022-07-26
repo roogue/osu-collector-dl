90ae70529989cd794e2771d2ef07619949e4fb92
roogue <57899698+roogue@users.noreply.github.com>
2022-07-26 09:19:46 +0800

    Enhance user experience

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,50 @@
 import Main from "./core/Main";
 import { config } from "./config";
 
-(async () => {
-  const prompt = require("prompt-sync")({ sigint: true });
-  const id = prompt("Please Enter An ID: ");
+// Prompt user for id and mode
+const prompt = require("prompt-sync")({ sigint: true });
 
+const getId = (): number | null => {
+  const id: number = Number(prompt("Please Enter An ID: "));
+  if (isNaN(id)) {
+    console.log("Invalid ID, please try again.");
+    return null;
+  }
+  return id;
+};
+
+const getMode = (): number | null => {
+  const mode: string = String(
+    prompt(
+      `Generate .osdb file? (y/n) - Default(${
+        config.mode === 2 ? "Yes" : "No"
+      }): `
+    )
+  );
+  // Select default mode if user doesn't enter anything
+  if (!mode) return config.mode;
+  // Check if user entered valid mode
+  if (["n", "no", "1"].includes(mode.toLowerCase())) return 1;
+  if (["y", "yes", "ass", "2"].includes(mode.toLowerCase())) return 2;
+
+  console.log('Invalid mode, please type "y" or "n".');
+  return null;
+};
+
+(async () => {
+  let id: number | null = null;
+  // Get id
+  while (id === null) {
+    id = getId();
+  }
+
+  let mode: number | null = null;
+  // Get mode
+  while (mode === null) {
+    mode = getMode();
+  }
+
+  config.mode = mode;
   const main = new Main(id, config);
 
   await main.run();
