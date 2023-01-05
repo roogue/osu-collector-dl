7ea260c709752186f1aa1ce5a3669a025999e52f
roogue <wsen4300@gmail.com>
2023-01-05 11:38:56 +0800

    More error message

--- src/struct/OcdlError.ts
+++ src/struct/OcdlError.ts
@@ -1,15 +1,16 @@
 export enum ErrorType {
   "GET_USER_INPUT_FAILED" = "Error occurred while getting user input",
   "RESOLVE_JSON_FAILED" = "Error occurred while resolving res body to json",
-  "REQUEST_DOWNLOAD_FAILED" = "Error occurred while requesting download",
+  "DOWNLOAD_FAILED" = "Error occurred while downloading beatmapset",
   "GENERATE_OSDB_FAILED" = "Error occurred while generating .osdb",
   "REQUEST_DATA_FAILED" = "Error occurred while requesting data",
   "FOLDER_GENERATION_FAILED" = "Error occurred while generating folder",
   "FILE_NAME_EXTRACTION_FAILED" = "Error occurred while extracting file name",
+  "MESSAGE_GENERATOR_FAILED" = "Error occurred while updating monitor",
+  "CORRUPTED_RESPONSE" = "The api response is corrupted",
 }
 
 const getMessage = (type: keyof typeof ErrorType, error: any): string => {
-  console.log(error);
   return `${new Date()} | [OcdlError]: ${type} - ${ErrorType[type]}\n${error}`;
 };
 
