5582c83bcf088f3df601e70a318560052a9ff3ef
roogue <wsen4300@gmail.com>
2023-01-08 20:31:16 +0800

    Create Constant.ts

---
+++ src/struct/Constant.ts
@@ -0,0 +1,7 @@
+export enum Constant {
+  OsuCollectorApiUrl = "https://osucollector.com/api/collections/",
+  OsuMirrorApiUrl = "https://api.chimu.moe/v1/download/",
+  OsuMirrorAltApiUrl = "https://kitsu.moe/api/d/",
+  GithubReleaseUrl = "https://github.com/roogue/osu-collector-dl/releases/tag/",
+  GithubReleaseApiUrl = "https://api.github.com/repos/roogue/osu-collector-dl/releases",
+}
