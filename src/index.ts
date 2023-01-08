4daeb90b0c10918a2c7231d96c58937800d0bd0a
roogue <wsen4300@gmail.com>
2023-01-08 18:20:31 +0800

    Combine function into worker

--- src/index.ts
+++ src/index.ts
@@ -1,80 +1,15 @@
-import Main from "./core/Main";
-import { config } from "./config";
+import Main from "./core/Worker";
 import Logger from "./core/Logger";
 import OcdlError from "./struct/OcdlError";
-import Monitor from "./core/Monitor";
-import { Message, Msg } from "./struct/Message";
-import { Collection } from "./struct/Collection";
-
-const isOnline = async (): Promise<boolean> => {
-  return !!(await require("dns")
-    .promises.resolve("google.com")
-    .catch(() => {}));
-};
 
 // Script Starts Here
 (async () => {
-  // Initiate monitor
-  const collection = new Collection();
-  const monitor = new Monitor(collection);
-  monitor.update();
-
-  // Check if internet connection is presence
-  const onlineStatus = await isOnline();
-  if (!onlineStatus)
-    return monitor.freeze(new Message(Msg.NO_CONNECTION).toString(), true);
-
-  let id: number | null = null;
-  let mode: number | null = null;
-
-  try {
-    // task 1
-    monitor.next();
-
-    // Get id
-    while (id === null) {
-      monitor.update();
-
-      const result = Number(
-        monitor.awaitInput(new Message(Msg.INPUT_ID).toString(), "none")
-      );
-      isNaN(result) ? (monitor.condition.retry_input = true) : (id = result); // check if result is valid
-    }
-
-    monitor.collection.id = id;
-
-    // task 2
-    monitor.next();
-
-    // Get mode
-    while (mode === null) {
-      monitor.update();
-      const result = String(
-        monitor.awaitInput(
-          new Message(Msg.INPUT_MODE, {
-            mode: config.mode === 2 ? "Yes" : "No",
-          }).toString(),
-          config.mode.toString()
-        )
-      );
-      if (["n", "no", "1"].includes(result)) mode = 1;
-      if (["y", "yes", "ass", "2"].includes(result)) mode = 2;
-      if (mode === null) monitor.condition.retry_mode = true;
-    }
-
-    monitor.setCondition({ mode: mode.toString() });
-    config.mode = mode;
-  } catch (e) {
-    Logger.generateErrorLog(new OcdlError("GET_USER_INPUT_FAILED", e));
-    return;
-  }
-
-  const main = new Main(monitor);
+  const main = new Main();
 
   try {
     await main.run();
   } catch (e) {
     if (e instanceof OcdlError) Logger.generateErrorLog(e);
-    monitor.freeze("An error occurred: " + e, true);
+    main.monitor.freeze("An error occurred: " + e, true);
   }
 })();
