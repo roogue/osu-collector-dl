f0825014ba3cdf984a281e77be273f305a2ac8f2
roogue <wsen4300@gmail.com>
2023-07-11 22:31:57 +0800

    Used queue system for downloads

--- src/core/DownloadManager.ts
+++ src/core/DownloadManager.ts
@@ -1,19 +1,22 @@
 import { createWriteStream, existsSync } from "fs";
-import { Response, fetch } from "undici";
+import { Response } from "undici";
 import _path from "path";
 import OcdlError from "../struct/OcdlError";
 import Util from "../util";
 import EventEmitter from "events";
-import type { BeatMapSet } from "../struct/BeatMapSet";
+import { BeatMapSet } from "../struct/BeatMapSet";
 import Manager from "./Manager";
-import { Constant } from "../struct/Constant";
 
+import PQueue from "p-queue";
+import { Requestor } from "./Requestor";
 // Define an interface for the events that the DownloadManager class can emit
 interface DownloadManagerEvents {
   downloaded: (beatMapSet: BeatMapSet) => void;
   error: (beatMapSet: BeatMapSet, e: unknown) => void;
   retrying: (beatMapSet: BeatMapSet) => void;
   downloading: (beatMapSet: BeatMapSet) => void;
+  // End is emitted along with un-downloaded beatmap
+  end: (beatMapSet: BeatMapSet[]) => void;
 }
 
 export declare interface DownloadManager extends Manager {
@@ -30,11 +33,11 @@ export declare interface DownloadManager extends Manager {
 
 export class DownloadManager extends EventEmitter implements DownloadManager {
   path: string;
-  // Whether to download beatmaps in parallel or sequentially
-  parallel: boolean;
-  // The number of beatmaps to download in parallel (if `parallel` is true)
-  concurrency: number;
-  not_downloaded: BeatMapSet[] = [];
+
+  // Queue for concurrency downloads
+  private queue: PQueue;
+  private downloadedBeatMapSetSize = 0;
+  private notDownloadedBeatMapSet: BeatMapSet[] = [];
 
   constructor() {
     super();
@@ -43,23 +46,28 @@ export class DownloadManager extends EventEmitter implements DownloadManager {
       Manager.config.directory,
       Manager.collection.getReplacedName()
     );
-    this.parallel = Manager.config.parallel;
-    this.concurrency = Manager.config.concurrency;
+
+    this.queue = new PQueue({
+      concurrency: Manager.config.parallel ? Manager.config.concurrency : 1,
+    });
   }
 
   // The primary method for downloading beatmaps
-  public async bulk_download(): Promise<BeatMapSet[]> {
-    // If `parallel` is true, download beatmaps in parallel using the `_impulse` method
-    if (this.parallel) {
-      await this._impulse();
-    } else {
-      // Otherwise, download beatmaps sequentially
-      for (const [, beatMapSet] of Manager.collection.beatMapSets) {
-        await this._downloadFile(beatMapSet);
-      }
-    }
+  public async bulkDownload(): Promise<void> {
+    // Add every download task to queue
+    Manager.collection.beatMapSets.forEach((beatMapSet) => {
+      this.queue.add(async () => await this._downloadFile(beatMapSet));
+    });
 
-    return this.not_downloaded;
+    // Emit if the download has been done
+    this.queue.on("idle", () => {
+      this.emit("end", this.notDownloadedBeatMapSet);
+    });
+    return;
+  }
+
+  getDownloadedBeatMapSetSize() {
+    return this.downloadedBeatMapSetSize;
   }
 
   // Downloads a single beatmap file
@@ -67,56 +75,51 @@ export class DownloadManager extends EventEmitter implements DownloadManager {
     beatMapSet: BeatMapSet,
     options: { retries: number; alt?: boolean } = { retries: 3 } // Whether or not use the alternative mirror url
   ): Promise<void> {
-    // Construct the URL for the beatmap file
-    const url =
-      (options.alt ? Constant.OsuMirrorAltApiUrl : Constant.OsuMirrorApiUrl) +
-      beatMapSet.id.toString();
-
     // Request the download
     try {
       this.emit("downloading", beatMapSet);
-
-      const res = await fetch(url, { method: "GET" });
-      if (!res.ok) throw `Status code: ${res.status}`;
-      // Extract the file name from the response headers
-      const fileName = this._getFilename(res);
       // Check if the specified directory exists
-      if (!this._checkIfDirectoryExists()) this.path = process.cwd();
-      // Create a write stream for the file
+      // This is placed here to prevent crashes while user editing folder
+      if (!this._checkIfDirectoryExists()) {
+        this.path = process.cwd();
+      }
 
+      const response = await Requestor.fetchDownloadCollection(beatMapSet.id);
+
+      const fileName = this._getFilename(response);
       const file = createWriteStream(_path.join(this.path, fileName));
-
-      // Write the file in chunks as the data is received
-      if (res.body) {
-        for await (const chunk of res.body) {
+      if (response.body) {
+        for await (const chunk of response.body) {
           file.write(chunk);
         }
       } else {
         throw "res.body is null";
       }
-
       file.end();
 
+      this.downloadedBeatMapSetSize++;
       this.emit("downloaded", beatMapSet);
     } catch (e) {
-      // If there are retries remaining, retry the download
+      // Retry the download with one fewer retry remaining, and use the alternative URL if this is the last retry
       if (options.retries) {
         this.emit("retrying", beatMapSet);
-        // Retry the download with one fewer retry remaining, and use the alternative URL if this is the last retry
+
         await this._downloadFile(beatMapSet, {
           alt: options.retries === 1,
           retries: options.retries - 1,
         });
       } else {
-        // If there are no retries remaining, emit the "error" event and add the beatmap to the list of failed downloads
+        // If there are no retries remaining,
+        // "error" event will be emitted,
+        // and the beatmap will be added to the list of failed downloads
         this.emit("error", beatMapSet, e);
-        this.not_downloaded.push(beatMapSet);
+        this.notDownloadedBeatMapSet.push(beatMapSet);
       }
     }
   }
 
-  private _getFilename(res: Response): string {
-    const headers = res.headers;
+  private _getFilename(response: Response): string {
+    const headers = response.headers;
     const contentDisposition = headers.get("content-disposition");
 
     let fileName = "Untitled.osz"; // Default file name
@@ -138,32 +141,6 @@ export class DownloadManager extends EventEmitter implements DownloadManager {
     }
 
     return fileName;
-  }
-
-  // Downloads beatmaps in parallel using the `concurrency` property to limit the number of concurrent downloads
-  private async _impulse(): Promise<void> {
-    const keys = Array.from(Manager.collection.beatMapSets.keys());
-    const loop_amount = Math.ceil(
-      Manager.collection.beatMapSets.size / this.concurrency
-    );
-
-    for (let i = 0; i < loop_amount; i++) {
-      const promises: Promise<void>[] = [];
-
-      // Calculate the range where the downloads should process
-      const start = i * this.concurrency;
-      const end = (i + 1) * this.concurrency;
-      const range = keys.slice(start, end);
-
-      // For the beatmap set on the range, push to a promise and download it in a burst
-      for (const id of range) {
-        const beatMapSet = Manager.collection.beatMapSets.get(id);
-        if (!beatMapSet) throw "beatMapSet is forbidden";
-        promises.push(this._downloadFile(beatMapSet));
-      }
-
-      await Promise.all(promises);
-    }
   }
 
   private _checkIfDirectoryExists(): boolean {
