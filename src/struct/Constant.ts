48c1bc086540d1ec21da3779d63ed06c93b9bb8f
roogue <wsen4300@gmail.com>
2023-08-27 19:05:41 +0800

    Switch to new osu mirror api

--- src/struct/Constant.ts
+++ src/struct/Constant.ts
@@ -1,7 +1,7 @@
 export enum Constant {
   OsuCollectorApiUrl = "https://osucollector.com/api/collections/",
   OsuMirrorApiUrl = "https://api.chimu.moe/v1/download/",
-  OsuMirrorAltApiUrl = "https://api.nerinyan.moe/d/",
+  OsuMirrorAltApiUrl = "https://osu.direct/api/d/",
   GithubReleaseUrl = "https://github.com/roogue/osu-collector-dl/releases/tag/",
   GithubReleaseApiUrl = "https://api.github.com/repos/roogue/osu-collector-dl/releases",
   OsuCollectorDbApiUrl = "https://ocdb.roogue.eu.org/collections/",
