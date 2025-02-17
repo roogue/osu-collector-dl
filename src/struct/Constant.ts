7af2788a115dba18980fb0b789f450be3350690d
roogue <wsen4300@gmail.com>
2025-02-18 00:27:32 +0800

    chore: added rate limit checker enpoint

--- src/struct/Constant.ts
+++ src/struct/Constant.ts
@@ -1,6 +1,7 @@
 export enum Constant {
   OsuCollectorApiUrl = "https://osucollector.com/api/collections/",
   OsuMirrorApiUrl = "https://catboy.best/d/",
+  OsuMirrorRateLimitUrl = "https://catboy.best/api/ratelimits",
   OsuMirrorAltApiUrl = "https://osu.direct/api/d/",
   GithubReleaseUrl = "https://github.com/roogue/osu-collector-dl/releases/tag/",
   GithubReleaseApiUrl = "https://api.github.com/repos/roogue/osu-collector-dl/releases",
